ed6beb2b710d68e3848f3a47dcf3023e86be6b71
zzunni <12201309@inha.edu>
2024-02-08 10:41:36 +0900

    v0.3 recursion preview

--- day20.py
+++ day20.py
@@ -1,8 +1,25 @@
-def factorial(number) -> int :
-    result = 1
-    for i in range(1, number+1):
-        result = result * i
-    return result
+#def factorial(number) -> int :
+#   '''
+#    factorial by reprtition
+#    :param number: number (int)
+#    :return: factorial result (int)
+#    '''
+#    result = 1
+#    for i in range(1, number+1):
+#        result = result * i
+#    return result
+
+def factorial(number) -> int:
+    '''
+    factorial by recursion
+    :param number: number (int)
+    :return: factorial result (int)
+    '''
+    if number == 1:
+        return 1
+    else:
+        return number * factorial(number - 1)
+
 
 def nCr(n, r) -> int :
     '''
@@ -17,6 +34,8 @@ def nCr(n, r) -> int :
 
 
 if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{r} = {nCr(n, r)}")
+    # n = int(input("Input n : "))
+    # r = int(input("Input r : "))
+    # print(f"{n}C{r} = {nCr(n, r)}")
+    f = int(input())
+    print(factorial(f))
