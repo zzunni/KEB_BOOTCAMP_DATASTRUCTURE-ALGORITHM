58d81b6c217a680ae3276c2e8217ce934d64c0ba
zzunni <12201309@inha.edu>
2024-02-08 16:04:59 +0900

    v0.8 poly

--- day20.py
+++ day20.py
@@ -1,10 +1,32 @@
-import mymath
-import time
+def print_poly(f_x) -> str:
+    term = len(f_x) - 1
+    poly_expression = "f(x) = "
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+
+        if coefficient >= 0:
+            poly_expression = poly_expression + "+"
+        poly_expression = poly_expression + f'{coefficient}x^{term} '
+        term = term - 1
+
+    return poly_expression
+
+
+def calculation_poly(x_value, f_x) -> int:
+    return_value = 0
+    term = len(f_x) - 1
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        return_value += coefficient * pow(x_value, term)
+        term = term - 1
+
+    return return_value
+
+
+fx = [2, 3, 4, 0, -9]
 
 if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{r} = {mymath.nCr(n, r)}")
-
-    # f = int(input())
-    # print(factorial(f))
+    print(print_poly(fx))
+    print(calculation_poly(int(input("x 값 : ")), fx))
--- number_game.py
+++ number_game.py
@@ -4,7 +4,7 @@ num = random.randint(0,100)
 chance = 10
 print(f"숫자맞추기 게임을 시작합니다. 주어진 기회는 {chance}번 입니다.")
 
-while True:
+while chance > 0:
     mynum = int(input("숫자를 맞춰보세요: "))
     if mynum < num:
         chance=chance-1
@@ -21,5 +21,6 @@ while True:
         print(f"정답을 {chance}번 만에 맞추셨습니다. ")
         break
 
-    elif chance == 0:
-        print(f"찬스를 모두 소진하셨습니다. 프로그램을 종료합니다.")
+    if chance == 0:
+        print(f"찬스를 모두 소진하셨습니다. 답은 {num}이었습니다. 프로그램을 종료합니다.")
+        break
